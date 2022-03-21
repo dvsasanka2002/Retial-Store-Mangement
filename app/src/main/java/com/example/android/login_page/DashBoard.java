2ac4a848a9dd7ad0e49b713e4c72401426ee1d80
bhargavmyadam <bhargavmyadam@gmail.com>
2022-03-21 17:25:05 +0530

    Third Commit DashBoard.

--- app/src/main/java/com/example/android/login_page/DashBoard.java
+++ app/src/main/java/com/example/android/login_page/DashBoard.java
@@ -2,13 +2,27 @@ package com.example.android.login_page;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DashBoard extends AppCompatActivity {
+
+    TextView mUserDetails;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+        mUserDetails = findViewById(R.id.tv_user_datails);
+        Intent intent = getIntent();
+        String username = intent.getStringExtra("username");
+        String phone = intent.getStringExtra("phone");
+        String email = intent.getStringExtra("email");
+        String location = intent.getStringExtra("location");
+        mUserDetails.setText("");
+        mUserDetails.append(username+"\n"+email +"\n"+location + "\n" + phone + "\n");
+
+
     }
 }
